d596119e9b9403cf2d9797aa7162f0a4f0d149ac
unknown <gamoscj123@gmail.com>
2022-04-08 11:25:06 +0800

    Client

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,13 @@ import { useState, useEffect } from "react"
 function App() {
   const [posts, setPosts] = useState([])
   const [loading, setLoading] = useState(false)
-  const [currentPage, setCurrentPage] = useState(1)
-  const [postsPerPage, setPostsPerPage] = useState(10)
 
   useEffect(() => {
+    // Fetch Data
     const fetchEmployee = async () => {
       setLoading(true)
       const res = await axios.get(
-        "https://3305-xscold-hackathondhvsut-snq9y95tirh.ws-us38.gitpod.ioapi/management/employee"
+        "https://3305-xscold-hackathondhvsut-emgtmtdcczy.ws-us38.gitpod.io/api/management/employee"
       )
       setPosts(res.data)
       setLoading(false)
@@ -40,18 +39,18 @@ function App() {
             <th>Sick Leave Credits</th>
             <th>Vacation Leave Credits</th>
             <th>Hourly Rate</th>
+            <th>Actions</th>
           </tr>
         </thead>
-        <tbody>
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
-        </tbody>
+        {posts?.length > 0 ? (
+          <tbody>
+            {posts.map((posts) => (
+              <Employee key={posts.id} posts={posts} loading={loading} />
+            ))}
+          </tbody>
+        ) : (
+          <h2>No posts Found</h2>
+        )}
       </Table>
     </div>
   )
--- client/src/components/Employee.js
+++ client/src/components/Employee.js
@@ -1,15 +1,27 @@
 import React from "react"
+import { Button } from "react-bootstrap"
+import "./Employee.css"
 
-function Employee() {
+function Employee({ posts, loading }) {
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
+
   return (
     <tr>
-      <td>1000</td>
-      <td>Vash</td>
-      <td>Ramos</td>
-      <td>Programmer</td>
-      <td>3</td>
-      <td>3</td>
-      <td>12000</td>
+      <td>{posts.id}</td>
+      <td>{posts.firstName}</td>
+      <td>{posts.lastName}</td>
+      <td>{posts.position}</td>
+      <td>{posts.sickLeaveCredits}</td>
+      <td>{posts.vacationLeaveCredits}</td>
+      <td>{posts.hourlyRate}</td>
+      <td>
+        <div className='employee-buttons'>
+          <Button variant='success'>Update</Button>{" "}
+          <Button variant='warning'>Delete</Button>{" "}
+        </div>
+      </td>
     </tr>
   )
 }
